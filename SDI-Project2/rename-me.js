9a08f1e602c43682e1840b5eea730f9dc00caed6
CurtisT1 <curtist2fullsail.edu>
2014-09-11 20:03:51 -0500

    added variables and prompt
    variables

--- SDI-Project2/rename-me.js
+++ SDI-Project2/rename-me.js
@@ -1,1 +1,41 @@
 alert("JavaScript works!");
+
+
+// Curtis Tate
+// SDI 1409
+// Assignment 2
+
+
+// My variables 
+
+
+var wizard1 = "Laxus";
+var wizard2 = "Warwick";
+var magicSource1 = "Staff";
+var magicSource2 = "Hands";
+var spell1 = "lighting Sky";
+var spell2 = "lighting fist strike";
+
+
+// output of my variables  
+
+
+console.log("Laxus and Warwick are battling it out to see who is stronger");
+console.log("" ,wizard1, " charged up his magic and released a big shock of wave of electricity to try to paralyze. ",wizard2, " quickly use his light magic to and conjure a barrier of light to protect him.");
+
+
+// Prompt For Input
+
+
+myPrompt = prompt("Have to choose the staff as magic source");
+
+if (myPrompt === "Staff") {
+	console.log("is powerful for defending attacks and attacking but it take time for his staff to charge and the staff slows him down");
+if (myPrompt === "Hands") {
+	console.log("Warwick hand magic is strong but he would have to use a lot of magic energy that will leave him unable to fight"); 
+} else {
+ 	console.log("",character2, "is ready to fight back");
+}; 
+} else {
+	console.log("",character2," hopefully he can beat laxus so he's going to give it everything hes got");
+};	
