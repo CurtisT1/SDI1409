ccc853efc51afedf7087dfeb3aafb272dd071424
CurtisT1 <curtist2fullsail.edu>
2014-09-18 21:00:05 -0500

    Added
    Added Variables

---
+++ SDI Project 3/rename-me.js
@@ -0,0 +1,20 @@
+alert("JavaScript works!");
+
+/* Curtis Tate
+SDI 1409
+Sept 18, 2014
+Assignment 3 */
+
+
+// variables
+
+
+var character1 = "Laxus";
+var character2 = "Warwick";
+var weapon1 = "Staff";
+var weapon2 = "Hands";
+var spell1 = "Lighting Sky";
+var spell2 = "Lighting Sky";
+
+
+
