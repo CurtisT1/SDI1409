e5b74d2b71d20ae38c0b1ba47399f330b053791c
CurtisT1 <curtist2fullsail.edu>
2014-09-25 21:24:43 -0500

    added another one
    added

--- SDI Project4/rename-me.js
+++ SDI Project4/rename-me.js
@@ -7,15 +7,24 @@ My library */
 
 var mylibrary = function(){
 
-//1.) does a string follow a 123-456-7890 pattern like a phone number?
+// 1. does a string follow a 123-456-7890 pattern like a phone number?
+
 varnumCheck = function (number){
 var total = number.length;
-dash = ".";
-adash = dash.indexof(".");
-bdash = dash.lastIndexof(".");
+dash = "-";
+adash = dash.indexof("-");
+bdash = dash.lastIndexof("-");
 	if (total === 12 || adash === 3 || bdash === 7) {
-	return true;
+		return true;
 	}else{
-		return false;
-	}
+		return false; 
+		}
+}
 }; // End of numCheck
+
+
+// 2.) format a number to use a specific number of a decimal place, as for money 2.1 -> 2.10
+var decimal = function (pi,afterdeci) {
+return number (pi.tofixed(afterdeci));
+
+};
